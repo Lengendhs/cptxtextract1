e40aaed62daf9c3d0ae636ae682536f586068aad
Lengendhs <166408647+Lengendhs@users.noreply.github.com>
2024-04-10 14:07:43 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,8 @@
 import os
-
-api_id = 4942197
-api_hash = "13248a2c551b73193969b42194023635"
-bot_token = "6913163552:AAHy8Tmi-HYELZkViq0rUVLQ7cTWjYJoiMk"
-auth_users = [5892781710]
+api_id = 22278147
+api_hash = "379775c2b8cda0ccf30784963f300111"
+bot_token = "7000679575:AAGbVmeO1cKpS4q57nKE2Cv7SlaKhV55Oso"
+auth_users = [6813146748]
 sudo_user = 5892781710
 log_channel = -1002073188657
 txt_channel = -1002001856626
